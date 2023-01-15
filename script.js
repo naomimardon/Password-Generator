ce8a88cb5e8f19720cafc009e8c4cc293e5c5869
Naomi Mardon <naomimardon@Naomis-MBP.broadband>
2023-01-15 19:22:56 +0000

    updated the function to prompt user for password options in JS, to generate new array of password characters based on user choices

--- script.js
+++ script.js
@@ -88,42 +88,71 @@ var upperCasedCharacters = [
   'Z'
 ];
 
+
+let criteriaArray = [];
+console.log("criteriaArray: " + criteriaArray);
+
+
 // Function to prompt user for password options
 function getPasswordOptions() {
-  let lengthPrompt = prompt("Type a number between 10 and 64 to choose the length of your password.\n\nThen click OK.");
-
-  if (!(lengthPrompt > 9) || !(lengthPrompt < 65)) {
+  let passwordLength = prompt("Type a number between 10 and 64 to choose the length of your password.\n\nThen click OK.");
+  console.log("passwordLength: " + Number(passwordLength));
+  if (!(passwordLength > 9) || !(passwordLength < 65) || !Number.isInteger(Number(passwordLength))) {
     alert("That is an invalid choice. Please try again.");
   } else {
-  let lowerConfirm = confirm("Would you like to include lower case letters (e.g abcde)?\n\nClick OK for Yes and Cancel for No.");
-  let upperConfirm =confirm("Would you like to include upper case letters (e.g. ABCDE)?\n\nClick OK for Yes and Cancel for No.");
-  let numberConfirm = confirm("Would you like to include numbers (e.g. 01234)?\n\nClick OK for Yes and Cancel for No.");
-  let characConfirm = confirm("Would you like to include special characters (e.g. $@%&*)?\n\nClick OK for Yes and Cancel for No.")
+    let specialConfirm = confirm("Would you like to include special characters (e.g. $@%&*)?\n\nClick OK for Yes and Cancel for No.")
+    console.log("specialConfirm: " + specialConfirm);
+      if (specialConfirm) {
+        criteriaArray = criteriaArray.concat(specialCharacters);
+      }
+
+    let numberConfirm = confirm("Would you like to include numbers (e.g. 01234)?\n\nClick OK for Yes and Cancel for No.");
+    console.log("numberConfirm: " + numberConfirm);
+      if (numberConfirm) {
+        criteriaArray = criteriaArray.concat(numericCharacters);
+      }
+
+    let lowerConfirm = confirm("Would you like to include lower case letters (e.g abcde)?\n\nClick OK for Yes and Cancel for No.");
+    console.log("lowerConfirm: " + lowerConfirm);
+      if (lowerConfirm) {
+        criteriaArray = criteriaArray.concat(lowerCasedCharacters);
+      }
+
+    let upperConfirm = confirm("Would you like to include upper case letters (e.g. ABCDE)?\n\nClick OK for Yes and Cancel for No.");
+    console.log("upperConfirm: " + upperConfirm);
+      if (upperConfirm) {
+        criteriaArray = criteriaArray.concat(upperCasedCharacters);
+      }
+  }
+
+  console.log(criteriaArray);
 }
-}
 
-// Function for getting a random element from an array
-function getRandom(arr) {
 
-}
+  // Function for getting a random element from an array
+  function getRandom(arr) {
+ 
+  }
 
-// Function to generate password with user input
-function generatePassword() {
-  getPasswordOptions();
-
+  // Function to generate password with user input
+  function generatePassword() {
+    getPasswordOptions();
   
-}
+  
 
-// Get references to the #generate element
-var generateBtn = document.querySelector('#generate');
+  }
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector('#password');
+  // Get references to the #generate element
+  var generateBtn = document.querySelector('#generate');
 
-  passwordText.value = password;
-}
+  // Write password to the #password input
+  function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector('#password');
 
-// Add event listener to generate button
-generateBtn.addEventListener('click', writePassword);
+    passwordText.value = password;
+  }
+
+  // Add event listener to generate button
+  generateBtn.addEventListener('click', writePassword);
+
