a31ee2edd75bbb1847cfe971b450e270361501a3
Naomi Mardon <naomimardon@Naomis-MBP.broadband>
2023-01-17 12:26:17 +0000

    created functions for repetitive tasks in JS

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Array of special characters to be included in password
-let specialCharacters = [
+var specialCharacters = [
   '@',
   '%',
   '+',
@@ -26,10 +26,10 @@ let specialCharacters = [
 ];
 
 // Array of numeric characters to be included in password
-let numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
 // Array of lowercase characters to be included in password
-let lowerCasedCharacters = [
+var lowerCasedCharacters = [
   'a',
   'b',
   'c',
@@ -59,7 +59,7 @@ let lowerCasedCharacters = [
 ];
 
 // Array of uppercase characters to be included in password
-let upperCasedCharacters = [
+var upperCasedCharacters = [
   'A',
   'B',
   'C',
@@ -88,94 +88,93 @@ let upperCasedCharacters = [
   'Z'
 ];
 
-
-
-
-
-function generatePassword() {
+// Function to select password options based on user choices
+function getPasswordOptions(arr) {
   let criteriaArray = [];
-  let passcode = [];
+  criteriaArray = criteriaArray.concat(arr);
+  console.log(criteriaArray);
+  return criteriaArray;
+}
 
+// Function for getting a random element from an array
+function getRandom(arr) {
+  let characterIndex = Math.floor(Math.random() * arr.length);
+  return arr[characterIndex];
+  
+}
+
+// Function to generate password with user input
+function generatePassword() {
+  let passcode = [];
+  let criteriaArray = [];
   let lengthPrompt = prompt("Type a number between 10 and 64 to choose the length of your password.\n\nThen click OK.");
   let passwordLength = parseFloat(lengthPrompt);
   console.log("passwordLength: " + passwordLength);
-  
+
   if (!(passwordLength > 9) || !(passwordLength < 65) || !Number.isInteger(passwordLength)) {
-    alert("That is an invalid choice. Please try again.");
+      alert("That is an invalid choice. Please try again.");
   } else {
-    
-    let specialConfirm = confirm("Would you like to include special characters (e.g. $@%&*)?\n\nClick OK for Yes and Cancel for No.")
-    console.log("specialConfirm: " + specialConfirm);
+
+      let specialConfirm = confirm("Would you like to include special characters (e.g. $@%&*)?\n\nClick OK for Yes and Cancel for No.")
+      console.log("specialConfirm: " + specialConfirm);
       if (specialConfirm) {
-        criteriaArray = criteriaArray.concat(specialCharacters);
-        let passwordCharacter  = specialCharacters[Math.floor(Math.random()*specialCharacters.length)];
-        passcode.push(passwordCharacter);
-        console.log(passcode);
+          criteriaArray = criteriaArray.concat(getPasswordOptions(specialCharacters));
+          passcode.push(getRandom(specialCharacters));
       }
 
-    let numberConfirm = confirm("Would you like to include numbers (e.g. 01234)?\n\nClick OK for Yes and Cancel for No.");
-    console.log("numberConfirm: " + numberConfirm);
+      let numberConfirm = confirm("Would you like to include numbers (e.g. 01234)?\n\nClick OK for Yes and Cancel for No.");
+      console.log("numberConfirm: " + numberConfirm);
       if (numberConfirm) {
-        criteriaArray = criteriaArray.concat(numericCharacters);
-        let passwordCharacter  = numericCharacters[Math.floor(Math.random()*numericCharacters.length)];
-        passcode.push(passwordCharacter);
-        console.log(passcode);
+          criteriaArray = criteriaArray.concat(getPasswordOptions(numericCharacters));
+          getRandom(numericCharacters);
+          passcode.push(getRandom(numericCharacters));
       }
 
-    let lowerConfirm = confirm("Would you like to include lower case letters (e.g abcde)?\n\nClick OK for Yes and Cancel for No.");
-    console.log("lowerConfirm: " + lowerConfirm);
+      let lowerConfirm = confirm("Would you like to include lower case letters (e.g abcde)?\n\nClick OK for Yes and Cancel for No.");
+      console.log("lowerConfirm: " + lowerConfirm);
       if (lowerConfirm) {
-        criteriaArray = criteriaArray.concat(lowerCasedCharacters);
-        let passwordCharacter  = lowerCasedCharacters[Math.floor(Math.random()*lowerCasedCharacters.length)];
-        passcode.push(passwordCharacter);
-        console.log(passcode);
+          criteriaArray = criteriaArray.concat(getPasswordOptions(lowerCasedCharacters));
+          passcode.push(getRandom(lowerCasedCharacters));
       }
 
-    let upperConfirm = confirm("Would you like to include upper case letters (e.g. ABCDE)?\n\nClick OK for Yes and Cancel for No.");
-    console.log("upperConfirm: " + upperConfirm);
+      let upperConfirm = confirm("Would you like to include upper case letters (e.g. ABCDE)?\n\nClick OK for Yes and Cancel for No.");
+      console.log("upperConfirm: " + upperConfirm);
       if (upperConfirm) {
-        criteriaArray = criteriaArray.concat(upperCasedCharacters);
-        let passwordCharacter  = upperCasedCharacters[Math.floor(Math.random()*upperCasedCharacters.length)];
-        passcode.push(passwordCharacter);
-        console.log(passcode);
+          criteriaArray = criteriaArray.concat(getPasswordOptions(upperCasedCharacters));
+          passcode.push(getRandom(upperCasedCharacters));
       }
   }
+  console.log("Passcode containing single chosen characters:" + passcode);
 
-    let passcodeLength = passcode.length;
-
-    console.log("Password Length: " + passwordLength);
-    console.log("Criteria Array: " + criteriaArray);
-    console.log("Passcode: " + passcode);
-    console.log("Passcode Length: " + passcodeLength);
-    console.log("passwordLength type: " + typeof passwordLength);
-    console.log("Passcode Length type: " + typeof passcodeLength);
-    console.log("Password Length - passcode.length: " + passwordLength --- passcodeLength);
+  let passcodeLength = passcode.length;
 
 
-    for (let i = 0; i < passwordLength-passcodeLength+1; i++) {
-      let passwordCharacter  = criteriaArray[Math.floor(Math.random()*criteriaArray.length)];
-      passcode.push(passwordCharacter)
-  }
-    console.log("passcode: " + passcode);
-    console.log("passcode.join: " + passcode.join(""));
-    let final = passcode.join("")
-    console.log("Final: " + final);
-    return final;
+  console.log("Criteria Array: " + criteriaArray);
+ 
+  console.log("Passcode Length containing single chosen characters: " + passcodeLength);
+
+
+
+  for (let i = 0; i < passwordLength-passcodeLength; i++) {
+      passcode.push(getRandom(criteriaArray));
+}
+  console.log("passcode: " + passcode);
+  let final = passcode.join("")
+  console.log("Final: " + final);
+  console.log(final.length);
+  return final;
 }
 
+// Get references to the #generate element
+var generateBtn = document.querySelector('#generate');
 
-  // Get references to the #generate element
-  let generateBtn = document.querySelector('#generate');
+// Write password to the #password input
+function writePassword() {
+var password = generatePassword();
+var passwordText = document.querySelector('#password');
 
-  // Write password to the #password input
-  function writePassword() {
-    let password = generatePassword();
-    let passwordText = document.querySelector('#password');
+passwordText.value = password;
+}
 
-    passwordText.value = password;
-  }
-
-  // Add event listener to generate button
-  generateBtn.addEventListener('click', writePassword);
-
- 
+// Add event listener to generate button
+generateBtn.addEventListener('click', writePassword);
